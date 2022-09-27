37f93c1c840b40a7307425ac0f121df61eb73eb8
Luis Alberto <33834755+albert509@users.noreply.github.com>
2022-09-27 09:28:32 -0400

    Deleted the dist folder because wa dont need it

--- dist/assets/404.21fba0bf.js
+++
@@ -1,1 +0,0 @@
-import{a as r,c as o,b as c,o as t,r as n}from"./index.29a49440.js";const s={},a={class:"w-full min-h-screen bg-blue-100 dark:bg-[#012A35] grid place-items-center"};function _(i,l){const e=n("router-view");return t(),o("main",a,[c(e)])}var d=r(s,[["render",_]]);export{d as default};
